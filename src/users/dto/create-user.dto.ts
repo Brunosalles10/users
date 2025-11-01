1111f90d27c473cf13fb9278b2f837d5813f4be4
Bruno Salles <bsalles428@gmail.com>
2025-11-01 16:24:06 -0300

    feat: initialize NestJS project with user management module
    - Add package.json with dependencies and scripts for NestJS application
    - Create AppController and AppService with basic "Hello World!" functionality
    - Implement UsersModule with UsersController and UsersService for user management
    - Define CreateUserDto and UpdateUserDto for user data validation
    - Create User entity for TypeORM integration
    - Add unit tests for AppController, UsersController, and UsersService
    - Implement e2e tests for the application
    - Configure TypeScript settings with tsconfig.json and tsconfig.build.json

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial.',
+    },
+  )
+  password: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+}
